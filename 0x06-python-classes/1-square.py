4ca7e57b742c619de00329226a348242031d4a9e
ibrotech01 <ibrowale3@gmail.com>
2023-03-28 13:48:30 -0700

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, __size):
+        """Constructorr.
+        Args:
+            size: length of square's side.
+        """
+        self.__size = __size
